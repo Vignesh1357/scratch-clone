b294e3ffe996274eecad5150821766e4cfd40c7a
Vignesh <vinayv1357@gmail.com>
2024-08-11 16:16:07 +0530

    feat: Implemented nav bar

--- src/App.js
+++ src/App.js
@@ -210,7 +210,10 @@ export default function App() {
           />{" "}
           <MidArea />
         </div>
-        <div className="w-1/3 h-screen overflow-hidden flex flex-row bg-white border-t border-l border-gray-200 rounded-tl-xl ml-2">
+        <div
+          style={{ height: "50%", borderRadius: "10px 10px" }}
+          className="w-1/3 h-screen overflow-hidden flex flex-row bg-white border-t border-l border-gray-200 rounded-tl-xl ml-2"
+        >
           <PreviewArea
             position={position}
             setPosition={setPosition}
--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "tailwindcss/tailwind.css";
-
-console.log("hi");
+import NavBar from "./components/NavBar";
 
 ReactDOM.render(
   <React.StrictMode>
+    <NavBar />
     <App />
   </React.StrictMode>,
   document.getElementById("root")
